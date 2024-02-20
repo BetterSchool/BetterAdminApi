92b2d7fd01c9bae10f2c25aeb5969cf20f17554d
Niko <Niko@DESKTOP-2R0EG79>
2024-02-20 13:41:22 +0100

    initial signupcontroller

---
+++ LogicApi/Controllers/SignUpController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LogicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        // POST api/<SignUpController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+    }
+}
